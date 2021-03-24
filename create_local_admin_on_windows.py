352b2be78061c4e28ab498380ccda5b2234b467c
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-03-24 08:38:38 -0700

    Upload script for Windows.

---
+++ create_local_admin_on_windows.py
@@ -0,0 +1,25 @@
+#!/bin/python
+
+# create local admin on Windows
+
+# import OS module 
+import os
+
+# declare localAdmin and localAdminPassword variables
+localAdmin = str(input("What is the local admin username you wish to create? (Example: local.admin)\n"))
+localAdminPassword = str(input("What the password you wish to create for this local admin? (Example: Password123)\n"))
+
+# create local admin account 
+createLocalAdmin = "net user /add {0} {1}".format(localAdmin, localAdminPassword)
+
+# add local admin to administrators group
+addLocalAdmintoAdminGroup = "net localgroup administrators {0} /add".format(localAdmin)
+
+# set local admin password to never expire 
+neverExpireLocalAdminPassword = "WMIC USERACCOUNT WHERE Name={0} SET PasswordExpires=FALSE".format(localAdmin)
+
+# define localAdminCreation list and for loop
+localAdminCreation = [createLocalAdmin, addLocalAdmintoAdminGroup, neverExpireLocalAdminPassword,'net user']
+
+for create in localAdminCreation: 
+    os.system(create)
