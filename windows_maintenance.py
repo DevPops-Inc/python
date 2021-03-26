102de60c5fb272030ff28f88fdffbcba22226f63
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-03-26 05:54:07 -0700

    Refactored maintenance script.

--- windows_maintenance.py
+++ windows_maintenance.py
@@ -5,16 +5,15 @@
 # import OS module 
 import os 
 
-# define maintenance list
+# prompt user input 
+str(input("Windows maintenance.\nPress any key to continue.\n"))
+
+# define maintenance list and for loop 
 maintenance = ['echo y | chkdsk /f/r c:', 'SFC /scannow', 'Dism /Online /Cleanup-Image /ScanHealth', 'defrag c: /u']
 
-# define maintenance for loop
 for job in maintenance: 
     os.system(job)
 
-# print message to user and wait for user input
-str(input("Please save your work and close applications. Press any key to restart Windows"))
-
-# restart Windows
+# prompt user save documents before restart
+str(input("Please save your work and close applications.\nPress any key to continue.\n"))
 os.system('shutdown /r /t 0')
-    
