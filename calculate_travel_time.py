443e6c7f9a119767ff1e9689e643491c6487592a
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-04-06 06:31:36 -0700

    Added user input.

--- calculate_travel_time.py
+++ calculate_travel_time.py
@@ -2,8 +2,11 @@
 
 # calculate travel time
 
-print("Input a rate and a distance")
-rate = float(input("Rate (Miles per Hour): "))
-distance = float(input("Distance (Miles): "))
+# prompt user input
+str(input("Calculate travel time.\nPress any key to continue.\n"))
+
+print("Input a rate and a distance\n")
+rate = float(input("Rate (Miles per Hour): \n"))
+distance = float(input("Distance (Miles): \n"))
 
 print("Time (Hours):", (distance / rate))
