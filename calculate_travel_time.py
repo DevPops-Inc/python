75a5065952daeac5c187c3bd4b16c16662b79325
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-04-07 05:51:13 -0700

    Added exit optionto script.

--- calculate_travel_time.py
+++ calculate_travel_time.py
@@ -3,7 +3,7 @@
 # calculate travel time
 
 # prompt user input
-str(input("Calculate travel time.\nPress any key to continue.\n"))
+str(input("Calculate travel time.\nPress any key to continue or press Ctrl and C keys to exit.\n"))
 
 print("Input a rate and a distance\n")
 rate = float(input("Rate (Miles per Hour): \n"))
