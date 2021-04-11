3e2434445414461e86c6cb0a1191e5d87209d6c9
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-04-11 07:24:56 -0700

    Added user input.

--- count_characters_in_last_name.py
+++ count_characters_in_last_name.py
@@ -2,6 +2,9 @@
 
 # count characters in last name
 
-name=input ("Enter your last name: ")
+# promot user input
+str(input("Count counters in last name.\nPress any key to to continue or press Ctrl and C keys to quit.\n"))
+
+name=str(input ("Enter your last name: "))
 count=len(name)
 print ("Your last name has", count, "letters in it.")
