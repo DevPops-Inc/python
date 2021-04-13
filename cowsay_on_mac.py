71620b2aee91e2263bcde8f3edf5ac6f3f7637f5
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-04-13 05:42:05 -0700

    Added user input.

--- cowsay_on_mac.py
+++ cowsay_on_mac.py
@@ -5,6 +5,9 @@
 # import OS module 
 import os
 
+# prompt user input 
+str(input("Cowsay on Mac.\nPress any key to continue or press Ctrl and C keys to quit.\n"))
+
 # declare cowMessgae and cmd variables 
 cowMessage = str(input("What would you like the cow to say?\n"))
 cmd = "cowsay {0}".format(cowMessage)
