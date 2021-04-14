c1237d5bd68aecb827c11c3a64f25c773ecebe70
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-04-14 06:55:28 -0700

    Added user input.

--- display_first_letter_in_name.py
+++ display_first_letter_in_name.py
@@ -2,6 +2,9 @@
 
 # display first letter in name 
 
+# prompt user input
+str(input("Display first letter in name.\nPress any key to continue or press Ctrl and C key to quit.\n"))
+
 # call user input with curly brackets 
 name=input("What's your name? ")
 print("Hello, {}.".format(name))
