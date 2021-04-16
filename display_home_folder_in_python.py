20a1d7321d20d883d51345f506a74e4c4532cd32
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-04-16 06:42:22 -0700

    Added user input and updated print statement.

--- display_home_folder_in_python.py
+++ display_home_folder_in_python.py
@@ -5,6 +5,9 @@
 # import OS module
 import os
 
+# prompt user for input
+str(input("\nDisplay home folder.\nPress any key to continue or press Ctrl and C keys to quit.\n"))
+
 # display home folder Python is running on
 home=os.getcwd()
-print(home)
+print("Your home folder is:", home, "\n")
