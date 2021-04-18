40ab672c29f5629e02b9ab14b5fd318fd62a4354
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-04-18 07:27:14 -0700

    Added user input, updated variables and prints.

--- four_letter_word_condition_example_in_python.py
+++ four_letter_word_condition_example_in_python.py
@@ -2,11 +2,17 @@
 
 # four letter word condition example in Python
 
-word=input("Please enter a four-letter word: ")
-word_length=len(word)
-if word_length == 4:
-    print(word, "is a four-letter world.  Well done.")
-elif word_length == 3:
-    print(word, "is a three-letter.  Try again")
+# prompt user input
+str(input("\nDetermine if a string is a four letter word.\nPress any key to continue or press Ctrl and C keys to quit."))
+
+# declare word and worldLength varibles
+word=str(input("\nPlease enter a four-letter word: \n"))
+wordLength=len(word)
+
+# if statements to determine four letter word condition
+if wordLength == 4:
+    print(word, "is a four-letter world.  Well done.\n")
+elif wordLength == 3:
+    print(word, "is a three-letter.  Try again\n")
 else:
-    print(word, "is not a four-letter word.")
+    print(word, "is not a four-letter word.\n")
