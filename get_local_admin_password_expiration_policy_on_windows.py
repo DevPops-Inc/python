1eccb4bc89c16ba6a7567b69b5b8d63bebf06c30
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-04-19 05:59:19 -0700

    Added exit option.

--- get_local_admin_password_expiration_policy_on_windows.py
+++ get_local_admin_password_expiration_policy_on_windows.py
@@ -6,7 +6,7 @@
 import os
 
 # prompt user input 
-str(input("Get local admin password expiration policy on Windows.\nPress any key to continue.\n"))
+str(input("\nGet local admin password expiration policy on Windows.\nPress any key to continue or press Ctrl and C keys to exit.\n"))
 
 # get local admin password expiration policy
 os.system('net user administrator | findstr /C:expires')
