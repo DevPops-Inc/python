996894a5fce4bde02c759fc9a993aee92909f4fe
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-04-20 06:35:22 -0700

    Added user input.

--- launch_chrome_on_mac_in_python.py
+++ launch_chrome_on_mac_in_python.py
@@ -1,7 +1,12 @@
 #!/bin/python
 
+# launch Chrome on Mac
+
 # import OS module
 import os
 
-# launch Chrome on Mac
+# prompt user input
+str(input("Launch Chrome on Mac.\nPress any key to continue or press control and C keys to quit.\n"))
+
+# launch Chrome
 browser=os.system('open -a "Google Chrome"')
