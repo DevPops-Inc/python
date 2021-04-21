b651c28357e91908cbbf4d16d68adce0ac068e63
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-04-21 05:46:16 -0700

    Added user input.

--- launch_chrome_on_windows_in_python.py
+++ launch_chrome_on_windows_in_python.py
@@ -1,7 +1,12 @@
 #!/bin/python
 
+# launch Chrome on Windows 
+
 # import OS module
 import os
 
+# prompt user input
+str(input("Launch Chrome on Windows.\nPress any key to continue or press Ctrl and C keys to quit.\n"))
+
 # launch Chrome on Windows
-wbrowser=os.system('start chrome')
+os.system('start chrome')
