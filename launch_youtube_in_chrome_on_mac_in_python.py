e43eff8c2c2a23762bf22f03f9b8e135e33a1bb1
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-04-22 06:01:35 -0700

    Added user input and fixed variable.

--- launch_youtube_in_chrome_on_mac_in_python.py
+++ launch_youtube_in_chrome_on_mac_in_python.py
@@ -1,11 +1,13 @@
 #!/bin/python
 
+# launch YouTube in Chrome on Mac
+
 # import OS module
 import os
 
-# launch YouTube in Chrome on Mac
-browser=os.system('open -a "Google Chrome" http://youtube.com')
+# prompt user input
+str(input("\nLaunch YouTube in Chrome on Mac.\nPress any key to continue or press control and C keys to quit.\n"))
 
-# use variable to open page
-a=('open -a "Google Chrome" http://youtube.com')
-os.system(a)
+# launch YouTube in Chrome
+youTubeInChrome=os.system('open -a "Google Chrome" http://youtube.com')
+os.system(youTubeInChrome)
