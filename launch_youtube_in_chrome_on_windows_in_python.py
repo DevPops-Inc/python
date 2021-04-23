a71f91b7845857947761eb056eff8863cd75196e
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-04-23 05:53:07 -0700

    Added user input and removed variable.

--- launch_youtube_in_chrome_on_windows_in_python.py
+++ launch_youtube_in_chrome_on_windows_in_python.py
@@ -1,7 +1,12 @@
 #!/bin/python
 
+# launch YouTube in Chrome on Windows 
+
 # import OS module 
 import os
 
-# launch YouTube in Chrome on Windows
-ytchromewin=os.system('start chrome "http://youtube.com"')
+# prompt user response
+str(input("\nLaunch YouTube in Chrome on Windows.\nPress any key to continue or press Ctrl and C keys to quit.\n"))
+
+# launch YouTube in Chrome
+os.system('start chrome "http://youtube.com"')
