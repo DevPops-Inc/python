e45af0d52f52750520d72f7c3315a31526194e34
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-04-25 07:41:17 -0700

    Added user input.

--- pass_python_variables_into_bash_command_example.py
+++ pass_python_variables_into_bash_command_example.py
@@ -5,10 +5,13 @@
 # import OS module 
 import os
 
+# prompt user input
+str(input("\nPass Python varibles into Bash command example.\nPress any key to continue or press control and C keys to quit.\n"))
+
 # declare cowMessgaes and cmd variables 
-cowMessage1 = str(input("What would you like the cow to say?\n"))
-cowMessage2 = str(input("Now what would you like the cow to say?\n"))
-cmd = "cowsay {0} {1}".format(cowMessage1, cowMessage2)
+cowMessage1 = str(input("\nWhat would you like the cow to say?\n"))
+cowMessage2 = str(input("\nNow what would you like the cow to say?\n"))
+cmd = "\ncowsay {0} {1}".format(cowMessage1, cowMessage2)
 
 # pass cmd variable into shell
 os.system(cmd)
