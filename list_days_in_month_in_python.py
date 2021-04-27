fecf69a33170e55a7a02e535397546f09a256be8
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-04-27 05:39:58 -0700

    Cleaned up syntax.

--- list_days_in_month_in_python.py
+++ list_days_in_month_in_python.py
@@ -5,11 +5,15 @@
 # import calendar module 
 import calendar
 
-# declare variables 
-year=int(input("What is the year? "))
-month=int(input("What is the month number? "))
+# prompt user input
+str(input("\nList number of days in a month.\nPress any key to continue or press Ctrl and C keys to quit.\n"))
 
+# declare year, month and cal variables 
+year=int(input("\nWhat is the year? "))
+month=int(input("\nWhat is the month number? "))
 cal=calendar.TextCalendar(calendar.SUNDAY)
-for i in cal.itermonthdays(year, month):
-    print(i)
+
+# define for loop and print days
+for days in cal.itermonthdays(year, month):
+    print(days)
     
