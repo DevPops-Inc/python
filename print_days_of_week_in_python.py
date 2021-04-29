3f4b85f0e0d8aaa2c88d204fb1603e57cff68f0a
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-04-29 06:02:18 -0700

    Added user input.

--- print_days_of_week_in_python.py
+++ print_days_of_week_in_python.py
@@ -1,8 +1,14 @@
 #!/bin/python
 
+# print days of the week 
+
 # import calendar module
 import calendar
+
+# prompt user input
+str(print("\nPrint days of the week.\nPress any key to continue or press Ctrl and C keys to quit.\n"))
 
 # print days of the week
 for name in calendar.day_name:
     print(name)
+    
