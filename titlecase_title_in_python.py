3d7d534e1172e554788b56a0fbbbf7cdeb39828e
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-04 06:36:51 -0700

    Fixed input statements.

--- titlecase_title_in_python.py
+++ titlecase_title_in_python.py
@@ -2,8 +2,11 @@
 
 # title case your title
 
-# declare variables
-title = str(input("type the title: (Example: foobar)"))
+# prompt user input
+str(input("\nTitle case your title.\nPress any key to continue or press Ctrl and C keys to quit.\n"))
+
+# declare title and x variables
+title = str(input("\nType the title: (Example: foobar)\n"))
 
 # capitalize title
 x = title.title()
