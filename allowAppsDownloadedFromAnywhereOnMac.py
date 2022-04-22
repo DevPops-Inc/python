80a5b9cc06d0f8728b189000e8e2b0410671df60
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-04-22 16:22:05 -0700

    Uploaded better script.

---
+++ allowAppsDownloadedFromAnywhereOnMac.py
@@ -0,0 +1,73 @@
+#!/bin/python 
+
+# allow apps downloaded from anywhere on Mac
+
+import colorama, os, sys, time, traceback
+from colorama import Fore, Style 
+from datetime import datetime
+colorama.init()
+
+def checkOsForMac(): 
+    print("Started checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+    if sys.platform == "darwin":
+        print("Operating System: " + Fore.GREEN)
+        print(os.system('sw_vers'))
+
+        print(Style.RESET_ALL + "Finished checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+        print("")
+    else:
+        print(Fore.RED + "Sorry but this script only runs on Mac.")
+        print(Style.RESET_ALL + "Finished checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+        
+        print("")
+        sys.exit()
+
+def allowAppsDownloadedFromAnywhere():
+    print("\nAllow apps downloaded from anywhere on Mac.\n")
+    checkOsForMac()
+
+    try:
+        start=datetime.now()
+        
+        print("Started allowing apps downloaded from anywhere at", start.strftime("%Y-%m-%d %H:%M %p")) 
+        
+        os.system('sudo spctl --master-disable')
+
+        print("\nExpand Apple menu and select \"System Preferences...\"")
+        input("Press any key to continue: ")
+        print("")
+
+        print("Select \"Security & Privacy.\"")
+        input("Press any key to continue: ")
+        print("")
+        
+        print("Select \"General\" tab and you will see \"Anywhere\" option under \"Allow apps downloaded from:\" list now.")
+        input("Press any key to continue: ")
+        print("")
+
+        print("Select the lock icon and log in with your credentials.")
+        input("Press any key to continue: ")
+        print("")
+
+        print("Select \"Anywhere\" to allow apps to be downloaded from anywhere on your Mac.")
+        input("Press any key to continue: ")
+        print("")
+
+        print(Fore.GREEN + "Successfully allowed apps downloaded from anywhere." + Style.RESET_ALL)
+
+        finished=datetime.now()
+        
+        print("Finished allowing apps downloaded from anywhere at", finished.strftime("%Y-%m-%d %H:%M %p"))
+
+        duration = finished - start
+        print("Total execution time: {0} second(s)".format(duration.seconds))
+
+    except Exception as e: 
+        print(Fore.RED + "Failed to allow apps downloaded from anywhere.")
+        print(e)
+        print(traceback.print_stack())
+        print("" + Style.RESET_ALL)
+
+allowAppsDownloadedFromAnywhere()
