85e37e5cfcaf41bf3d9bdcd06a322f215170b896
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-05-11 18:20:37 -0700

    Uploaded script for checking SSH status on Mac.

---
+++ checkSshStatusOnMac.py
@@ -0,0 +1,53 @@
+#!/bin/python
+
+# check SSH status on Mac
+
+import colorama, os, sys, traceback
+from colorama import Fore, Style
+from datetime import datetime
+colorama.init()
+
+def checkOsForMac(): 
+    print("Started checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+    if sys.platform == "darwin":
+        print(Fore.GREEN + "Operating System: ")
+        print(os.system('sw_vers'))
+        print(Style.RESET_ALL)
+
+        print("Finished checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+        print("")
+    else:
+        print(Fore.RED + "Sorry but this script only runs on macOS." + Style.RESET_ALL)
+
+        print("Finished checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+        exit("")
+
+
+def checkSshStatus(): 
+    print("\nCheck SSH status on Mac.\n")
+    checkOsForMac()
+
+    try: 
+        startDateTime = datetime.now()
+        print("Started checking SSH at", startDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+        os.system('sudo systemsetup -getremotelogin')
+        print(Fore.GREEN + "Successfully checked SSH status." + Style.RESET_ALL)
+
+        finishedDateTime = datetime.now()
+        
+        print("Finished checking SSSh at", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+        duration = finishedDateTime - startDateTime
+        print("Total execution time: {0}".format(duration.seconds))
+        print("")
+    except Exception as e: 
+        print(Fore.RED + "Failed to check SSH status.")
+        print(e)
+        print(traceback.print_stack)
+        exit("" + Style.RESET_ALL)
+
+checkSshStatus()
