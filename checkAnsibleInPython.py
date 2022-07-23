36d9d0d8f2c4ddd7b4bcaf0dfe0b8f6e95a2864c
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-07-23 08:43:36 -0700

    Upload checking Ansible script.

---
+++ checkAnsibleInPython.py
@@ -0,0 +1,104 @@
+#!/bin/python
+
+# check Ansible in Python 
+
+import colorama, os, sys, subprocess, traceback
+from colorama import Fore, Style
+from datetime import datetime
+colorama.init()
+
+def checkOs():
+    print("Started checking operating system at", datetime.now().strftime("%Y-%m-"))
+
+    if sys.platform == "win32": 
+        print(Fore.GREEN + "Operating System: ")
+        print(os.system('ver'))
+        print(Style.RESET_ALL)
+        operatingSystem = "Windows"
+    elif sys.platform == "darwin": 
+        print(Fore.GREEN + "Operating System: ")
+        print(os.system('sw_vers'))
+        print(Style.RESET_ALL)
+        operatingSystem = "macOS"
+    elif sys.platform == "linux": 
+        print(Fore.GREEN + "Operating System: ")
+        print(os.system('uname -r'))
+        print(Style.RESET_ALL)
+        operatingSystem = "Linux"
+
+    print("Finished checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+    print("")
+    return operatingSystem
+
+def checkAnsible(): 
+    print("\nCheck Ansible in Python.\n")
+    operatingSystem = checkOs()
+
+    try:
+        startDateTime = datetime.now()
+        
+        print("Started checking Ansible at", startDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+        FNULL = open(os.devnull,'w')
+
+        if operatingSystem == "macOS" or operatingSystem == "Linux":
+
+            checkAnsibleOnMacOrLinux = subprocess.call(['which', 'ansible'], stdout=FNULL) 
+
+            if checkAnsibleOnMacOrLinux == 0:
+                print(Fore.GREEN + "Ansible is installed."+ Style.RESET_ALL)
+                os.system('ansible --version')
+                print(Fore.GREEN + "Successfully checked Ansible." + Style.RESET_ALL)
+
+                finishedDateTime = datetime.now()
+
+                print("Finished checking Ansible at", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+                duration = finishedDateTime - startDateTime
+                print("Total execution time: {0} second(s)".format(duration.seconds))
+                print("")
+            else: 
+                print(Fore.RED + "Ansible is not installed." + Style.RESET_ALL)
+                
+                finishedDateTime = datetime.now()
+
+                print("Finished checking Ansible at", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+                duration = finishedDateTime - startDateTime
+                print("Total execution time: {0} second(s)".format(duration.seconds))
+                exit("")
+
+        elif operatingSystem == "Windows": 
+            
+            checkAnsibleOnWindows = subprocess.call(['where', 'ansible'], stdout=FNULL)
+
+            if checkAnsibleOnWindows == 0:
+                print(Fore.GREEN + "Ansible is installed."+ Style.RESET_ALL)
+                os.system('ansible --version')
+                print(Fore.GREEN + "Successfully checked Ansible." + Style.RESET_ALL)
+
+                finishedDateTime = datetime.now()
+
+                print("Finished checking Ansible at", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+                duration = finishedDateTime - startDateTime
+                print("Total execution time: {0} second(s)".format(duration.seconds))
+                print("")
+            else: 
+                print(Fore.RED + "Ansible is not installed." + Style.RESET_ALL)
+                
+                finishedDateTime = datetime.now()
+
+                print("Finished checking Ansible at", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+                duration = finishedDateTime - startDateTime
+                print("Total execution time: {0} second(s)".format(duration.seconds))
+                exit("")
+    except Exception as e: 
+        print(Fore.RED + "Failed to check Ansible in Python.")
+        print(e)
+        print(traceback.print_stack)
+        exit("" + Style.RESET_ALL)
+
+checkAnsible()
