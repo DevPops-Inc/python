4e1f3678f5e4fd553808f3751c6cd44a877b910d
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-07-29 15:15:26 -0700

    Cleaned up spacing.

--- calculateTravelTimeInPython.py
+++ calculateTravelTimeInPython.py
@@ -15,11 +15,13 @@ def checkOs():
         print(os.system('ver'))
         print(Style.RESET_ALL)
         operatingSystem = "Windows"
+
     elif sys.platform == "darwin": 
         print(Fore.GREEN + "Operating System: ")
         print(os.system('sw_vers'))
         print(Style.RESET_ALL)
         operatingSystem = "macOS"
+
     elif sys.platform == "linux": 
         print(Fore.GREEN + "Operating System: ")
         print(os.system('uname -r'))
@@ -36,15 +38,18 @@ def getRate(operatingSystem):
         rate = float(input("Please type the rate (miles per hour) and press \"Enter\" key (Example: 45): "))
 
         print("")
+
     elif operatingSystem == "macOS" or operatingSystem == "Linux": 
         rate = float(input("Please type the rate (miles per hour) and press \"return\" key (Example: 45): "))
 
         print("")
+
     return rate 
 
 def getDistance(operatingSystem):
     if operatingSystem == "Windows": 
         distance = float(input("Please type the distance (miles) and press \"Enter\" key (Example: 100): "))
+
     elif operatingSystem == "macOS" or operatingSystem == "Linux": 
         distance = float(input("Please type the distance (miles) and press \"return\" key (Example: 100): "))
 
@@ -76,6 +81,7 @@ def checkParameters(rate, distance):
         print("Finished checking parameters at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
 
         print("")
+
     else: 
         print(Fore.RED + "One or more parameter checks are incorrect." + Style.RESET_ALL)
 
@@ -90,6 +96,7 @@ def calculateTravelTime():
     if len(sys.argv) > 2: 
         rate     = float(sys.argv[1])
         distance = float(sys.argv[2])
+
     else: 
         rate = getRate(operatingSystem)
         distance = getDistance(operatingSystem)
@@ -111,6 +118,7 @@ def calculateTravelTime():
         duration = finishedDateTime - startDateTime
         print("Total execution time: {0} seconds".format(duration.seconds))
         print("")
+        
     except Exception as e:
         print(Fore.RED + "Failed to calculate travel time" + Style.RESET_ALL)
         print(e)
