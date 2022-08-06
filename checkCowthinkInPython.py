5003854640249c695b04336225488e5145f8f1d2
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-08-06 06:12:00 -0700

    Upload Cowthink script.

---
+++ checkCowthinkInPython.py
@@ -0,0 +1,107 @@
+#!/bin/python
+
+# check Cowthink in Python
+
+import colorama, os, sys, subprocess, traceback
+from colorama import Fore, Style
+from datetime import datetime
+colorama.init()
+
+def checkOs():
+    print("Started checking operating system at", datetime.now().strftime("%Y-%m-"))
+
+    if sys.platform == "win32": 
+        print(Fore.GREEN + "Operating System: ")
+        print(os.system('ver'))
+        print(Style.RESET_ALL)
+        operatingSystem = "Windows"
+
+    elif sys.platform == "darwin": 
+        print(Fore.GREEN + "Operating System: ")
+        print(os.system('sw_vers'))
+        print(Style.RESET_ALL)
+        operatingSystem = "macOS"
+
+    elif sys.platform == "linux": 
+        print(Fore.GREEN + "Operating System: ")
+        print(os.system('uname -r'))
+        print(Style.RESET_ALL)
+        operatingSystem = "Linux"
+
+    print("Finished checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+    print("")
+    return operatingSystem
+
+def checkCowthink(): 
+    print("\nCheck Cowthink in Python.\n")
+    operatingSystem = checkOs()
+
+    try:
+        startDateTime = datetime.now()
+        
+        print("Started checking Cowthink at", startDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+        FNULL = open(os.devnull,'w')
+
+        if operatingSystem == "macOS" or operatingSystem == "Linux":
+
+            checkCowthinkOnMacOrLinux = subprocess.call(['which', 'cowthink'], stdout=FNULL)
+
+            if checkCowthinkOnMacOrLinux == 0:
+                os.system('cowthink "Cowthink is installed"')
+                print(Fore.GREEN + "Successfully checked cowthink." + Style.RESET_ALL)
+
+                finishedDateTime = datetime.now()
+
+                print("Finished checking Cowthink at", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+                duration = finishedDateTime - startDateTime
+                print("Total execution time: {0} second(s)".format(duration.seconds))
+                print("")
+
+            else: 
+                print(Fore.RED + "Cowthink is not installed." + Style.RESET_ALL)
+                
+                finishedDateTime = datetime.now()
+
+                print("Finished checking Cowthink at", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+                duration = finishedDateTime - startDateTime
+                print("Total execution time: {0} second(s)".format(duration.seconds))
+                exit("")
+
+        elif operatingSystem == "Windows": 
+            
+            checkCowthinkOnWindows = subprocess.call(['where', 'cowthink'], stdout=FNULL) 
+            
+            if checkCowthinkOnWindows == 0:
+                os.system('cowthink "Cowthink is installed"')
+                print(Fore.GREEN + "Successfully checked cowthink." + Style.RESET_ALL)
+
+                finishedDateTime = datetime.now()
+
+                print("Finished checking Cowthink at", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+                duration = finishedDateTime - startDateTime
+                print("Total execution time: {0} second(s)".format(duration.seconds))
+                print("")
+                
+            else: 
+                print(Fore.RED + "Cowthink is not installed." + Style.RESET_ALL)
+                
+                finishedDateTime = datetime.now()
+
+                print("Finished checking Cowthink at", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+                duration = finishedDateTime - startDateTime
+                print("Total execution time: {0} second(s)".format(duration.seconds))
+                exit("")
+
+    except Exception as e: 
+        print(Fore.RED + "Failed to check Cowthink in Python.")
+        print(e)
+        print(traceback.print_stack)
+        exit("" + Style.RESET_ALL)
+
+checkCowthink()
