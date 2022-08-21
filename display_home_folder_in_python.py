8193a48bbacaf6ddc537f19f083c452125af7823
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-08-21 07:52:30 -0700

    Refactored script to be more programmatic.

--- display_home_folder_in_python.py
+++ display_home_folder_in_python.py
@@ -1,13 +1,62 @@
 #!/bin/python
 
-# display home folder
+# display home directory
 
-# import OS module
-import os
+import colorama, os, sys, traceback
+from colorama import Fore, Style
+from datetime import datetime
+colorama.init()
 
-# prompt user for input
-str(input("\nDisplay home folder.\nPress any key to continue or press Ctrl and C keys to quit.\n"))
+def checkOs(): 
+    print("Started checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
 
-# display home folder Python is running on
-home=os.getcwd()
-print("Your home folder is:", home, "\n")
+    if sys.platform == "win32": 
+        print(Fore.GREEN + "Operating System: ")
+        print(os.system('ver'))
+        print(Style.RESET_ALL)
+        operatingSystem = "Windows"
+
+    elif sys.platform == "darwin": 
+        print(Fore.GREEN + "Operating System: ")
+        print(os.system('sw_vers'))
+        print(Style.RESET_ALL)
+        operatingSystem = "macOS"
+
+    elif sys.platform == "linux": 
+        print(Fore.GREEN + "Operating System: ")
+        print(os.system('uname -r'))
+        print(Style.RESET_ALL)
+        operatingSystem = "Linux"
+
+    print("Finished checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+    
+    print("")
+    return operatingSystem
+
+def displayHomeDir(): 
+    print("\nDisplay directory directory.\n")
+    operatingSystem = checkOs()
+
+    try: 
+        startDateTime = datetime.now()
+        print("Started displaying home directory at", startDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+        homeDir=os.getcwd()
+        print(Fore.BLUE + "Your home directory is: {0}".format(homeDir))
+        print(Fore.GREEN + "Successfully displayed home directory." + Style.RESET_ALL)
+
+        finishedDateTime = datetime.now()
+        
+        print("Finished displaying home directory at", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+        duration = finishedDateTime - startDateTime
+        print("Total execution time: {0} second(s)".format(duration.seconds))
+        print("")
+    
+    except Exception as e: 
+        print(Fore.RED + "Failed to display home directory.")
+        print(e)
+        print(traceback.print_stack)
+        exit("" + Style.RESET_ALL)
+
+displayHomeDir()
