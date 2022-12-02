a7c2f7518a2576cd4d1dbeb36e01433616c1d30c
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-12-02 06:54:15 -0800

    Refactored script programmatically.

--- print_one_char_at_a_time_vertically.py
+++ print_one_char_at_a_time_vertically.py
@@ -1,22 +1,115 @@
 #!/bin/python
 
-# print one char at a time vertically
+# progressively print vertical string 
 
-# import module
-import time
+import colorama, os, sys, time, traceback
+from colorama import Fore, Style
+from datetime import datetime
+colorama.init()
 
-# prompt user input
-str(input("\nPrint one char at a time vertically.\nPress any key to continue or press Ctrl and C keys to quit.\n"))
 
-# declare verticalString variable 
-verticialString = str(input("\nWhat would you like to print vertically?\n"))
+def checkOs(): 
+    print("Started checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
 
-# define function
-def delayPrint(verticialString):
-    # define for loop
-    for char in verticialString: 
-        print("\n", char)
-        time.sleep(.25)
+    if sys.platform == "win32": 
+        print(Fore.GREEN + "Operating System:", end="")
+        os.system('ver')
+        print(Style.RESET_ALL, end="")
+        operatingSystem = "Windows"
+
+    elif sys.platform == "darwin": 
+        print(Fore.GREEN + "Operating System:")
+        os.system('sw_vers')
+        print(Style.RESET_ALL, end="")
+        operatingSystem = "macOS"
+        
+    elif sys.platform == "linux": 
+        print(Fore.GREEN + "Operating System:")
+        os.system('uname -r')
+        print(Style.RESET_ALL, end="")
+        operatingSystem = "Linux"
+
+    print("Finished checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+    print("")
+    return operatingSystem
+
+
+def getVerticalString(operatingSystem): 
+    if operatingSystem == "Windows": 
+        verticalString = str(input("Please type the string you would like to print vertically and press the \"Enter\" key (Example: Python is fun!): " ))  
+
+        print("")
+
+    else: 
+        verticalString = str(input("Please type the string you would like to print vertically and press the \"Enter\" key (Example: Python is fun!): " ))  
+
+        print("")
+
+    return verticalString
+
+
+def checkParameters(verticalString): 
+    print("Started checking parameter(s) at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+    valid = "true"
+
+    print("Parameter(s):")
+    print("------------------------------------------")
+    print("verticalString: {0}".format(verticalString))
+    print("------------------------------------------")
+
+    if verticalString == None: 
+        print(Fore.RED + "Vertical string is not set." + Style.RESET_ALL)
+        valid = "false"
+
+    if valid == "true": 
+        print(Fore.GREEN + "All parameter check(s) passed." + Style.RESET_ALL)
+
+        print("Finished checking parameter(s) at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+        print("")
+
+    else: 
+        print(Fore.RED + "One or more parameter checks are incorrect." + Style.RESET_ALL)
+
+        print("Finished checking parameter(s) at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+        exit("")
+
+
+def progressivelyPrintVerticalString(): 
+    print("Progressively print vertical string in Python.\n")
+    operatingSystem = checkOs()
+
+    if len(sys.argv) >= 2: 
+        verticalString = sys.argv[1]
+
+    else: 
+        verticalString = getVerticalString(operatingSystem)
+
+    checkParameters(verticalString)
+
+    try: 
+        startDateTime = datetime.now()
+
+        print("Started progressively printing {0} at {1}".format(verticalString, startDateTime.strftime("%Y-%m-%d %H:%M %p")))
+
+        for char in verticalString: 
+            print(Fore.BLUE + char)
+            time.sleep(.25)
+
+        print(Fore.GREEN + "Succesfully printed {0} progressively.".format(verticalString) + Style.RESET_ALL)
+
+        finishedDateTime = datetime.now()
+
+        print("Finished progressively printing {0} at {1}".format(verticalString, finishedDateTime.strftime("%Y-%m-%d %H:%M %p")))
+
+        duration = finishedDateTime - startDateTime
+        print("Total execution time: {0} second(s)".format(duration.seconds))
+        print("")
+
+    except Exception as e: 
+        print(Fore.RED + "Failed to progressively print {0}".format(verticalString))
+        print(e)
+        print(traceback.print_stack)
+        exit("" + Style.RESET_ALL)
     
-# call function and pass in verticialString
-delayPrint(verticialString)
+
+progressivelyPrintVerticalString()
