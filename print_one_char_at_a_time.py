fe6e9d4f1f12fbd2934d965dc14a7f99e3fefcd4
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-12-03 06:31:14 -0800

    Refactored script programmatically.

--- print_one_char_at_a_time.py
+++ print_one_char_at_a_time.py
@@ -1,22 +1,113 @@
 #!/bin/python
 
-# print char at a time
+# progessively print chars in Python
 
-# import time and sys modules 
-import time, sys
+# you can run this script with: python3 progressivelyPrintCharsInPython.py '< input string >'
 
-# prompt user input
-str(input("\nPrint char at a time.\nPress any key to continue or press Ctrl and C keys to quit.\n"))
+import colorama, os, sys, time, traceback
+from colorama import Fore, Style 
+from datetime import datetime
+colorama.init()
 
-# declare inputString variable 
-inputString = str(input("What would you like to print one char at a time?\n"))
 
-# define function
-def delayPrint(inputString):
-    for char in inputString: 
-        sys.stdout.write(char)
-        sys.stdout.flush()
-        time.sleep(0.25)
+def checkOs(): 
+    print("Started checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
 
-# call function and pass in inputString
-delayPrint(inputString)
+    if sys.platform == "win32": 
+        print(Fore.GREEN + "Operating System:", end="")
+        os.system('ver')
+        print(Style.RESET_ALL, end="")
+        operatingSystem = "Windows"
+
+    elif sys.platform == "darwin": 
+        print(Fore.GREEN + "Operating System:")
+        os.system('sw_vers')
+        print(Style.RESET_ALL, end="")
+        operatingSystem = "macOS"
+
+    elif sys.platform == "linux": 
+        print(Fore.GREEN + "Operating System:")
+        os.system('uname -r')
+        print(Style.RESET_ALL, end="")
+        operatingSystem = "Linux"
+
+    print("Finished checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+    print("")
+    return operatingSystem
+
+
+def getInputString(operatingSystem): 
+    if operatingSystem == "Windows": 
+        inputString = str(input("Please type an input string and press the \"Enter\" key (Example: Python rocks!): "))
+
+    else: 
+        inputString = str(input("Please type an input string and press the \"return\" key (Example: Python rocks!): "))
+
+    print("")
+    return inputString
+
+
+def checkParameters(inputString): 
+    print("Started checking parameter(s) at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+    valid = "true"
+
+    print("Parameter(s):")
+    print("------------------------------------")
+    print("inputString: {0}".format(inputString))
+    print("------------------------------------")
+
+    if valid == "true": 
+        print(Fore.GREEN + "All parameter check(s) passed." + Style.RESET_ALL)
+        
+        print("Finished checking parameters at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+        print("")
+
+    else: 
+        print(Fore.RED + "One or more parameters are incorrect." + Style.RESET_ALL)
+
+        print("Finished checking parameters at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+        exit("")
+
+
+def progressivelyPrintChars(): 
+    print("\nProgressively print chars at in Python.\n")
+    operatingSystem = checkOs()
+
+    if len(sys.argv) >= 2: 
+        inputString = str(sys.argv[1])
+
+    else: 
+        inputString = getInputString(operatingSystem)
+
+    checkParameters(inputString)
+
+    try: 
+        startDateTime = datetime.now()
+        print("Started progressively printing chars at", startDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+        print(Fore.BLUE, end="")
+
+        for char in inputString: 
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(0.25)
+
+        print("")
+        print(Fore.GREEN + "Successfully printed chars progressively." + Style.RESET_ALL)
+        
+        finishedDateTime = datetime.now()
+        
+        print("Finished progressively printing chars at", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+        duration = finishedDateTime - startDateTime
+        print("Total execution time: {0} second(s)".format(duration.seconds))
+        print("")
+
+    except Exception as e: 
+        print(Fore.RED + "Failed to progressivey print chars.")
+        print(e)
+        print(traceback.print_stack)
+        exit("" + Style.RESET_ALL)
+
+
+progressivelyPrintChars()
