7582bf750aba1ad39166b72d6c5ac0703100db77
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-12-04 07:48:40 -0800

    Refactored script programmatically.

--- titlecase_title_in_python.py
+++ titlecase_title_in_python.py
@@ -2,14 +2,107 @@
 
 # title case your title
 
-# prompt user input
-str(input("\nTitle case your title.\nPress any key to continue or press Ctrl and C keys to quit.\n"))
+# you can run this script with: python3 titleCaseStringInPython.py '< title string >'
 
-# declare title and x variables
-title = str(input("\nType the title: (Example: foobar)\n"))
+import colorama, os, sys, traceback
+from colorama import Fore, Style 
+from datetime import datetime 
+colorama.init()
 
-# capitalize title
-x = title.title()
 
-# print title
-print(x)
+def checkOs(): 
+    print("Started checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+    if sys.platform == "win32": 
+        print(Fore.GREEN + "Operating System:", end="")
+        os.system('ver')
+        print(Style.RESET_ALL, end="")
+        operatingSystem = "Windows"
+
+    elif sys.platform == "darwin": 
+        print(Fore.GREEN + "Operating System:")
+        os.system('sw_vers')
+        print(Style.RESET_ALL, end="")
+        operatingSystem = "macOS"
+
+    elif sys.platform == "linux":
+        print(Fore.GREEN + "Operating System:")
+        os.system('uname -r')
+        print(Style.RESET_ALL, end="")
+        operatingSystem = "Linux"
+
+    print("Finished checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+    print("")
+    return operatingSystem
+
+
+def getTitleString(operatingSystem): 
+    if operatingSystem == "Windows": 
+        titleString = str(input("Please type the string you wish to title case and press the \"Enter\" key (Example: python rocks!): "))
+
+    else: 
+        titleString = str(input("Please type the string you wish to title case and press the \"return\" key (Example: python rocks!): "))
+
+    print("")
+    return titleString
+
+
+def checkParameters(titleString): 
+    print("Started checking parameter(s) at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+    valid = "true"
+
+    print("Parameter(s):")
+    print("------------------------------------")
+    print("titleString: {0}".format(titleString))
+    print("------------------------------------")
+
+    if valid == "true": 
+        print(Fore.GREEN + "All parameter check(s) passed." + Style.RESET_ALL)
+
+        print("Finished checking parameter(s) at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+        print("")
+
+    else: 
+        print(Fore.RED + "One or more parameter checks passed." + Style.RESET_ALL)
+
+        print("Finished checking parameter(s) at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+        exit("")
+
+
+def titleCaseString(): 
+    print("\nTitle case string in Python.\n")
+    operatingSystem = checkOs()
+
+    if len(sys.argv) >= 2: 
+        titleString = str(sys.argv[1])
+
+    else: 
+        titleString = getTitleString(operatingSystem)
+
+    checkParameters(titleString) 
+
+    try: 
+        startDateTime = datetime.now()
+        
+        print("Started title casing \"{0}\" at {1}".format(titleString, startDateTime.strftime("%Y-%m-%d %H:%M %p")))
+
+        print(Fore.BLUE + "\"{0}\" is now \"{1}\"".format(titleString, titleString.title()))
+
+        print(Fore.GREEN + "Successfully title cased \"{0}\"".format(titleString.title()) + Style.RESET_ALL)
+
+        finishedDateTime = datetime.now()
+
+        print("Finished title casing \"{0}\" at {1}".format(titleString.title(), finishedDateTime.strftime("%Y-%m-%d %H:%M %p")))
+
+        duration = finishedDateTime - startDateTime
+        print("Total execution time: {0} second(s)".format(duration.seconds))
+        print("")
+
+    except Exception as e: 
+        print(Fore.RED + "Failed to title case \"{0}\"".format(titleString))
+        print(e)
+        print(traceback.print_stack)
+        exit("" + Style.RESET_ALL)
+
+
+titleCaseString()
