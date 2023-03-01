8a837a34dc49b734f6c1f0d087add9fac73dda3c
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2023-03-01 05:07:23 -0800

    Cleaned spacing in parameter checks.

--- calculateTravelTimeInPython.py
+++ calculateTravelTimeInPython.py
@@ -60,7 +60,6 @@ def getDistance(operatingSystem):
 
 def checkParameters(rate, distance): 
     print("Started checking parameter(s) at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
-
     valid = True
 
     print("Parameter(s):")
@@ -81,14 +80,12 @@ def checkParameters(rate, distance):
         print(Fore.GREEN + "All parameter check(s) passed." + Style.RESET_ALL)
 
         print("Finished checking parameter(s) at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
-
         print("")
 
     else: 
         print(Fore.RED + "One or more parameter checks are incorrect." + Style.RESET_ALL)
 
         print("Finished checking parameter(s) at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
-
         exit("")
 
 
