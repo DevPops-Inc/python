60245dfd0be25dfa42c9ab80c03eff024e7f49b9
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2023-03-13 06:01:24 -0700

    Cleaned spacing in main function.

--- checkESpeakInPython.py
+++ checkESpeakInPython.py
@@ -40,13 +40,11 @@ def checkESpeak():
 
     try:
         startDateTime = datetime.now()
-        
         print("Started checking eSpeak at", startDateTime.strftime("%m-%d-%Y %I:%M %p"))
 
         FNULL = open(os.devnull,  'w')
 
         if operatingSystem == "macOS" or operatingSystem == "Linux":
-
             checkESpeakOnMacOrLinux = subprocess.call(['which', 'espeak'], stdout=FNULL) 
 
             if checkESpeakOnMacOrLinux == 0:
@@ -74,7 +72,6 @@ def checkESpeak():
                 exit("")
 
         elif operatingSystem == "Windows": 
-            
             checkESpeakOnWindows = subprocess.call(['where', 'espeak'], stdout=FNULL)
 
             if checkESpeakOnWindows == 0:
@@ -103,7 +100,6 @@ def checkESpeak():
                 
     except Exception: 
         print(Fore.RED + "Failed to check eSpeak in Python.")
-        
         traceback.print_exc()
         exit("" + Style.RESET_ALL)
 
