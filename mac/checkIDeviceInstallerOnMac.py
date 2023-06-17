c878eecc938a9f42d0744e091da356dc2cfde6b3
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2023-06-17 05:44:47 -0700

    Cleaned spacing in exception block.

--- mac/checkIDeviceInstallerOnMac.py
+++ mac/checkIDeviceInstallerOnMac.py
@@ -65,7 +65,6 @@ def checkIDeviceInstaller():
         
     except Exception: 
         print(Fore.RED + "Failed to check ideviceinstaller.")
-        
         traceback.print_exc()
         exit("" + Style.RESET_ALL)
         
