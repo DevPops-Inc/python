b80f2a0a6de0dbd18bee2e476f44f36e752c26d4
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2023-06-21 05:59:22 -0700

    Added comment that I haven't tested this script yet.

--- mac/connectToWifiOnMac.py
+++ mac/connectToWifiOnMac.py
@@ -3,6 +3,7 @@
 # connect to Wi-Fi on Mac
 
 # you can run this script with: python3 connectToWifiOnMac.py < SSID > < password >
+# haven't tested this script yet
 
 import colorama, getpass, os, sys, traceback
 from colorama import Fore, Style
