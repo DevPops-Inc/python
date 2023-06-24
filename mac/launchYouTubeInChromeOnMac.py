31eebe52c94d3ff57a43ef907258097d15297dfb
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2023-06-24 07:00:54 -0700

    Cleaned spacing in exception block.

--- mac/launchYouTubeInChromeOnMac.py
+++ mac/launchYouTubeInChromeOnMac.py
@@ -73,7 +73,6 @@ def launchYouTubeInChrome():
 
     except Exception: 
         print(Fore.RED + "Failed to launch YouTube in Chrome.")
-        
         traceback.print_exc()
         exit("" + Style.RESET_ALL)
 
