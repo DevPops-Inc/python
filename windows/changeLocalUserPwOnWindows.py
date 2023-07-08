6479567afe2c1dce57dfc2f7fa79eb0f527737b2
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2023-07-08 06:36:26 -0700

    Cleaned spacing in OS check.

--- windows/changeLocalUserPwOnWindows.py
+++ windows/changeLocalUserPwOnWindows.py
@@ -19,12 +19,14 @@ def checkOsForWindows():
         print(Style.RESET_ALL, end="")
         
         print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
+
         print("")
 
     else:
         print(Fore.RED + "Sorry this script only works on Windows." + Style.RESET_ALL)
     
         print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
+        
         exit("")
 
 
